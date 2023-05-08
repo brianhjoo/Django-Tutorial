18108a6e174a026c2002f38348af660019ad6b11
sarah <sarahgraup@g.ucla.edu>
2023-05-08 15:13:29 -0700

    finished part 7- created admin page and form

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db import models
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
@@ -5,6 +7,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 
@@ -14,7 +17,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
     # latest_question_list = Question.objects.order_by("-pub_date")[:5]
     # context = {"latest_question_list": latest_question_list}
@@ -31,6 +34,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def detail(request, question_id):
 #     # try:
